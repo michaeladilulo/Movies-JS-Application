b289965cb61c03d3c89be39d06df5c80649b7ef4
michaeladilulo <michaeladilulo@gmail.com>
2023-09-05 08:26:24 -0600

    Search Term Input parameter with filtering

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get(`http://www.omdbapi.com/`, {
         params: {
             apikey: '74aeddd9',
-            i: "tt2455546"
+            s: searchTerm
         }
     });
 
     console.log(response.data);
-}
+};
 
-fetchData();
+const input = document.querySelector('input');
+input.addEventListener('input', (event) => {
+fetchData(event.target.value);
+})
