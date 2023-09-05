7921b680d5a2f3a545aad5ff145ee93bd785cd04
michaeladilulo <michaeladilulo@gmail.com>
2023-09-05 08:49:42 -0600

    Using debounce function in call, notes on apply

--- app.js
+++ app.js
@@ -9,16 +9,24 @@ const fetchData = async (searchTerm) => {
     console.log(response.data);
 };
 
-let timeoutId;
-const input = document.querySelector('input');
-const onInput = event => {
+const debounce = (func) => {
+    let timeoutId;
+return (...args) => { // might need to receive arguments, args need to be forwarded on to 'func'
     if(timeoutId) {
         clearTimeout(timeoutId);
     }
-    
+
     timeoutId = setTimeout(() => {
-        fetchData(event.target.value);
-    }, 1000);
-}
+        func.apply(null, args); // apply: call function as normally would, take arguments, pass in as separate arguments to the function
+        // apply will automatically keep track of arguments being passed through
+    }, 1000)
+
+ };
+};
+
+const input = document.querySelector('input');
+const onInput = debounce(event => {
+    fetchData(event.target.value);
+});
 
 input.addEventListener('input', onInput);
