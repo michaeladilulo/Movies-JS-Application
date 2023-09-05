6e8326b4af463e01786a75450216b228551f8d7b
michaeladilulo <michaeladilulo@gmail.com>
2023-09-05 08:13:44 -0600

    Making Request for a single movie

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fetchData = async () => {
     const response = await axios.get(`http://www.omdbapi.com/`, {
         params: {
             apikey: '74aeddd9',
-            s: 'avengers'
+            i: "tt2455546"
         }
     });
 
