5e81f4cb7831d7468e89b9c6c5842e3b782e247c
michaeladilulo <michaeladilulo@gmail.com>
2023-09-05 08:09:00 -0600

    Receive response from API

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+const fetchData = async () => {
+    const response = await axios.get(`http://www.omdbapi.com/`, {
+        params: {
+            apikey: '74aeddd9',
+            s: 'avengers'
+        }
+    });
+
+    console.log(response.data);
+}
+
+fetchData();
