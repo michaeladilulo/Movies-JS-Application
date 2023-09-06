eccef56e66194da03582c44ddde739b77d031273
michaeladilulo <michaeladilulo@gmail.com>
2023-09-05 18:47:28 -0600

    Making a request for a movie by ID

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ const onInput = async event => {
         option.addEventListener('click', (event) => {
             dropdown.classList.remove('is-active')
             input.value = movie.Title;
+            onMovieSelect(movie);
         })
 
         resultsWrapper.appendChild(option);
@@ -65,3 +66,14 @@ document.addEventListener('click', (event) => {
         dropdown.classList.remove('is-active');
     }
 })
+
+const onMovieSelect = async movie => {
+    const response = await axios.get(`http://www.omdbapi.com/`, {
+        params: {
+            apikey: '74aeddd9',
+            i: movie.imdbID
+        }
+    });
+
+    console.log(response.data)
+}
