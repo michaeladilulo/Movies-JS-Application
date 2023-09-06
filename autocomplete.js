40147c8ff7abaeb322094aa2bcfe1a74056bf230
michaeladilulo <michaeladilulo@gmail.com>
2023-09-05 21:55:57 -0600

    Extracting Selection Logic: 251 refactor

--- autocomplete.js
+++ autocomplete.js
@@ -1,4 +1,4 @@
-const createAutoComplete = ({ root, renderOption }) => {
+const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue }) => {
     root.innerHTML = `
     <label><b>Search For A Movie</b></label>
     <input class="input" />
@@ -33,8 +33,8 @@ const createAutoComplete = ({ root, renderOption }) => {
     
             option.addEventListener('click', (event) => {
                 dropdown.classList.remove('is-active')
-                input.value = movie.Title;
-                onMovieSelect(movie);
+                input.value = inputValue(movie);
+                onOptionSelect(movie);
             })
     
             resultsWrapper.appendChild(option);
